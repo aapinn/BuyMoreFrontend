70e850ec2c54f1ab8f0faa00df430e079e8244d2
ArifRh9185 <arifrh9185@gmail.com>
2025-12-29 21:43:24 +0700

    fixed env

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,6 @@
+const API_URL = import.meta.env.VITE_API_URL;
+
 export async function getProducts() {
-  const res = await fetch("http://localhost:3000/products");
+  const res = await fetch(`${API_URL}/products`);
   return res.json();
 }
